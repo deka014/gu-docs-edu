58b556d4964f8ce288c48973effa2cf9f70d08b8
Deep <deepjyotideka8@gmail.com>
2021-12-09 19:30:21 +0530

    set react router and homepage folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import Dashboard from './components/Dashboard'
+import Homepage from './homepage/Homepage'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
-  return <Dashboard />
+  return (
+    <div>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/navbar' element={<Homepage />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../navbar/Navbar'
+
+function Homepage() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Homepage
